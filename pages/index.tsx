6105bf781cfe1001d14bd11b9ec4c4c2200f2e84
Daniel BG <daniel@bettrads.com>
2021-09-21 05:29:17 -0300

    ft/fetch-popular

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,20 @@ import styles from '../styles/Home.module.css'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Post from "../components/Post";
-export default function IndexPage() {
+export default function IndexPage({ posts }) {
   return (
     <>
       <Header />
-      <Post />
+
+      <Post posts={posts.results} />
       <Footer />
     </>
   );
 }
 
+
+IndexPage.getInitialProps = async (ctx) => {
+  const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=45c857b10f1d7b5f1b33127149d88893')
+  const posts = await res.json()
+  return { posts }
+}
